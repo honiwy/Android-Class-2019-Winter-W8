4851954e808425881ba9948da11a66925ae29baf
WYC <honiwy@gmail.com>
2020-01-01 07:15:05 -0500

    Another version test of firebase

--- 2020/app/src/main/java/student/rachel/firebase2020/MainActivity.kt
+++ 2020/app/src/main/java/student/rachel/firebase2020/MainActivity.kt
@@ -1,32 +1,30 @@
-package student.rachel.firebase
+package student.rachel.firebase2020
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 
-
 class MainActivity : AppCompatActivity() {
-    lateinit var db: DocumentReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        db = FirebaseFirestore.getInstance().document("IceCreams/ja")
+        var db = FirebaseFirestore.getInstance().document("IceCreams/test")
         var flavour = "banana"
 
 
         val items = HashMap<String, Any>()
-        items["Ingrediant-1"] = "butter"
+        items["Ingrediant-1"] = "butterfly"
         items["Ingrediant-2"] = "water"
         db.collection(flavour).document("Ingrediants").set(items)
             .addOnSuccessListener {
-                void: Void? -> Toast.makeText(this, "Successfully uploaded to the database :)", Toast.LENGTH_LONG).show()
-        }.addOnFailureListener {
-                exception: java.lang.Exception -> Toast.makeText(this, exception.toString(), Toast.LENGTH_LONG).show()
-        }
+                Log.i("apple","success")
+                Toast.makeText(this, "Successfully uploaded to the database :)", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {exception->
+                Log.i("apple","fail")
+                Toast.makeText(this, exception.toString(), Toast.LENGTH_LONG).show()
+            }
     }
 }
-
